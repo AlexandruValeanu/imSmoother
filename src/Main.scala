4753264f5e021bdf6d1d2ebfa769c13019156146
Alexandru Văleanu <alexandru.valeanu@oriel.ox.ac.uk>
2017-04-15 17:40:22 +0300

    update Main.scala

--- src/Main.scala
+++ src/Main.scala
@@ -1,3 +1,5 @@
+// Concurrent image smoothing application written in Scala
+
 object Main {
   def main(args: Array[String]): Unit = {
     SmoothUtils.time(SmoothUtils.smoothImage("tom_jerry.png"))
